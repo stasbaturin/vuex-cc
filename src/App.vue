bd2043b42f95368f73a213f1f6517f5462a1a79b
sbaturin <sbaturin@vm-ub2204-srv1.sbat.pro>
2023-03-25 00:43:34 +0300

    add a list of posts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="post" v-for="post of posts" :key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async mounted() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=3')
+    const posts = await res.json()
+    this.posts = posts
   }
 }
 </script>
@@ -23,6 +30,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  width: 400px;
+}
+
+.post {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 1rem;
 }
 </style>
